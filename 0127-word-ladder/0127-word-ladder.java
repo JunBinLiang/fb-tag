9b436ea528fefc278b504b7d77fac11d312c4ed9
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-11-24 16:59:26 -0500

    Time: 1090 ms (12.30%), Space: 45.2 MB (73.76%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -4,19 +4,16 @@ class Solution {
         if(s.equals(t)) {
             return 0;
         }
+        
         int n = a.size();
         a.add(s);
-        List<Integer> g[] = new ArrayList[n + 1];
-        Arrays.setAll(g, e -> new ArrayList<>());
-        
-        for(int i = 0; i < a.size(); i++) {
-            for(int j = i + 1; j < a.size(); j++) {
-                if(connect(a.get(i), a.get(j))) {
-                    g[i].add(j);
-                    g[j].add(i);
-                }
-            }
+        Set<Integer> frontSet = new HashSet<>();
+        Set<Integer> endSet = new HashSet<>();
+        for(int i = 0; i < n; i++) {
+            frontSet.add(i);
+            endSet.add(i);
         }
+        
         
         Queue<Integer> frontQ = new LinkedList<>();
         Queue<Integer> endQ = new LinkedList<>();
@@ -24,10 +21,12 @@ class Solution {
         Arrays.fill(d1, INF); Arrays.fill(d2, INF);
         d1[n] = 1;
         frontQ.add(n);
+        
         for(int i = 0; i < n; i++) {
             if(a.get(i).equals(t)) {
                 endQ.add(i);
                 d2[i] = 1;
+                endSet.remove(i);
                 break;
             }
         }
@@ -36,30 +35,40 @@ class Solution {
         int res = INF;
         while(frontQ.size() > 0 && endQ.size() > 0) {
             Queue<Integer> curQ;
+            Set<Integer> curS, otherS;
             int D1[], D2[];
             if(times % 2 == 1) {
                 curQ = frontQ;
+                curS = frontSet;
+                otherS = endSet;
                 D1 = d1;
                 D2 = d2;
             } else {
                 curQ = endQ;
+                curS = endSet;
+                otherS = frontSet;
                 D1 = d2;
                 D2 = d1;
             }
             
             int u = curQ.poll();
-            for(int nxt : g[u]) {
-                if(D1[nxt] == INF && D2[nxt] == INF) {
-                    D1[nxt] = 1 + D1[u];
-                    curQ.add(nxt);
+            List<Integer> adds = new ArrayList<>();
+            for(int nxt : curS) {
+                if(!connect(a.get(u), a.get(nxt))) continue;
+                if(!otherS.contains(nxt)) {
+                    //System.out.println(u + "  " + nxt + "     |" + D1[u] + "  "+ D2[nxt]);
+                    res = Math.min(res, D1[u] + D2[nxt]);
                     continue;
                 }
-                
-                if(D2[nxt] != INF) {
-                    res = Math.min(res, D1[u] + D2[nxt]);
-                }
-                
+                D1[nxt] = 1 + D1[u];
+                curQ.add(nxt);
+                adds.add(nxt);
             }
+            
+            for(int add : adds) {
+                curS.remove(add);
+            }
+            
             times++;
         }
         
