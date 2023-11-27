ce27d020738c8a2844a91908d63f3c593d4d177f
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-11-27 01:46:22 -0500

    Time: 0 ms (100.00%), Space: 45.5 MB (23.13%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -20,12 +20,12 @@ class Solution {
         }
         if(key < root.val) {
             root.left = deleteNode(root.left, key);
-        } else if(key > root.val){
+        } else if(key > root.val) {
             root.right = deleteNode(root.right, key);
         } else{
-            if(root.left == null){
+            if(root.left == null) {
                 return root.right;
-            } else if(root.right == null){
+            } else if(root.right == null) {
                 return root.left;
             }
             TreeNode minNode = findMin(root.right);
