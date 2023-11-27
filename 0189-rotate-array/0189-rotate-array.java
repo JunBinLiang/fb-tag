80be3af2954132eb296cd07774bd56eda0b9f8ad
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-11-27 04:57:44 -0500

    Time: 3 ms (8.77%), Space: 55.5 MB (67.56%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        if(k <= n - k) {
+            reverse(nums, 0, n - k - 1);
+            //System.out.println(Arrays.toString(nums));
+            for(int i = 0; i < k; i++) {
+                int t = nums[i];
+                nums[i] = nums[n - k + i];
+                nums[n - k + i] = t;
+            }
+            //System.out.println(Arrays.toString(nums));
+            reverse(nums, n - k , n - 1);
+            //System.out.println(Arrays.toString(nums));
+            reverse(nums, k, n - k - 1);
+            //System.out.println(Arrays.toString(nums));
+        } else {
+            k = n - k;
+            reverse(nums, n - k, n - 1);
+            //System.out.println(Arrays.toString(nums));
+            reverse(nums, k, n - k - 1);
+            //System.out.println(Arrays.toString(nums));
+            for(int i = 0; i < k; i++) {
+                int t = nums[i];
+                nums[i] = nums[n - k + i];
+                nums[n - k + i] = t;
+            }
+            reverse(nums, 0, n - k - 1);
+            //System.out.println(Arrays.toString(nums));
+        }
+    }
+    
+    public void reverse(int nums[], int l, int r) {
+        while(l < r) {
+            int t = nums[l];
+            nums[l] = nums[r];
+            nums[r] = t;
+            l++;
+            r--;
+        }
+    }
+}
