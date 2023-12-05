2d5cf251be043e35f680e7d073ed8cda8b9567ae
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-12-05 14:25:50 -0500

    Time: 3 ms (78.88%), Space: 40.9 MB (17.57%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -1,28 +1,38 @@
 class Solution {
     public String smallestSubsequence(String s) {
-        Stack<Character> stk = new Stack<>();
-        int count1[] = new int[26];
-        int count2[] = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            count1[s.charAt(i) - 'a']++;
+        int[] lastOccurrence = new int[26]; // Assuming only lowercase English letters
+
+        // Populate the last occurrence index for each character in the string
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence[s.charAt(i) - 'a'] = i;
         }
-        
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            while(stk.size() > 0 && count2[c - 'a'] == 0 && c <= stk.peek() && count1[stk.peek() - 'a'] > 0) {
-                count2[stk.pop() - 'a']--;
+
+        boolean[] used = new boolean[26];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            // Skip characters that are already in the result
+            if (used[currentChar - 'a']) {
+                continue;
             }
-            
-            if(count2[c - 'a'] == 0) {
-                count2[c - 'a']++;
-                stk.push(c);
+
+            // Pop characters from the stack if the current character is smaller and has a later occurrence
+            while (!stack.isEmpty() && currentChar < stack.peek() && i < lastOccurrence[stack.peek() - 'a']) {
+                used[stack.pop() - 'a'] = false;
             }
-            count1[c - 'a']--;
+
+            stack.push(currentChar);
+            used[currentChar - 'a'] = true;
         }
-        
-        StringBuilder str = new StringBuilder();
-        while(stk.size() > 0) str.append(stk.pop());
-        return str.reverse().toString();
+
+        // Build the result string from the stack
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
