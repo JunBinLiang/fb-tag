faae73b61395e5bc1ca428d0e48a481df4fd2464
66brother <45537132+JunBinLiang@users.noreply.github.com>
2024-01-10 08:23:11 -0500

    Time: 113 ms (41.86%), Space: 56.6 MB (25.58%) - LeetHub

---
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIncreasingGroups(List<Integer> usageLimits) {
+        Collections.sort(usageLimits);
+        int n = usageLimits.size();
+        int res = 0; long count = 0;
+        for(int x : usageLimits) {
+            count += x;
+            if(count >= (res + 1l) * (res + 2) / 2) res++;
+        }
+        return res;
+    }
+    
+    
+}
